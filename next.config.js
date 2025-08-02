0e9209351a706311575df4da5c7f03a07742c10e
lexx-project <passlexyganteng@gmail.com>
2025-08-02 20:29:12 +0700

    FIX THUMBNAIL

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.imgur.com",
       },
+      {
+        protocol: "https",
+        hostname: "otakudesu.best",
+      },
     ],
   },
   async headers() {
